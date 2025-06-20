ef136f690c3e84de2f71a73a48c19a6d6a33ef0d
gv <gv@MacBook-Pro-de-gv.local>
2025-06-20 14:30:15 +0200

    add virtual disk drive

--- run.sh
+++ run.sh
@@ -5,4 +5,10 @@ set -xue
 QEMU=qemu-system-riscv32
 
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
+#$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
+
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -d unimp,guest_errors,int,cpu_reset -D qemu.log \
+    -drive id=drive0,file=lorem.txt,format=raw,if=none \
+    -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
+    -kernel kernel.elf
